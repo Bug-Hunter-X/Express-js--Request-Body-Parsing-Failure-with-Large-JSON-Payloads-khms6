33618fa4d091064cdcdc3041610e76ed4d15b971
BugHunter-X <askzenai@gmail.com>
2025-02-06 04:12:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(express.json({ limit: '50mb' })); // Added limit for larger JSON payloads
+app.post('/data', (req, res) => {
+  const data = req.body;
+  // Process the data
+  console.log(data); 
+  res.send('Data received');
+});
+app.listen(3000, () => console.log('Server listening on port 3000'));
